011baa76fed5a7a079d42e07f391f485aa409660
Andre Grant <andrethegrant@gmail.com>
2021-12-12 15:24:43 -0500

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <nav id="button-container">
+          <button className="about-button">
+            <Link to="/about">About Me</Link>
+          </button>
+          <button className="project-button">
+            <Link to="/projects">Projects</Link>
+          </button>
+          <button className="contact-button">
+            <Link to="/contact">Contact Me</Link>
+          </button>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+      ;{" "}
+    </Router>
   );
 }
 
